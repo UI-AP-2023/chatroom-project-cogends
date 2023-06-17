6f0c634f8f9f0a91d165f0ccee260e22d5ea5bd7
ferreshteh <fereshteh.khademi1381@gmail.com>
2023-06-17 09:57:07 +0330

    edit client

---
+++ ClientSocket/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket=new Socket("127.0.0.1",6666);
+        System.out.println("connected");
+        Scanner sc=new Scanner(System.in);
+        PrintWriter out=new PrintWriter(clientSocket.getOutputStream(),true);
+        String input="";
+        do{
+            input=sc.nextLine();
+            out.println(input);
+        }
+        while (!Objects.equals(input, "finish"));
+        System.out.println("proses finish ");
+        out.close();
+
+    }
+}
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("waiting ");
+        ServerSocket serverSocket=new ServerSocket(6666);
+        Socket clientSocket=serverSocket.accept();
+        System.out.println("server accepted");
+        InputStreamReader reader=new InputStreamReader(clientSocket.getInputStream());
+        BufferedReader in=new BufferedReader(reader);
+        String answer="";
+        // System.out.println(answer);
+        while (( answer=in.readLine())!=null) {
+            if(answer.equals("finish")){
+                answer += "tamom shoood";
+                System.out.println(answer);
+                break;
+            }
+            else {
+                answer += "  --> Etelaei Nadaram! ";
+                System.out.println(answer);}
+        }
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
