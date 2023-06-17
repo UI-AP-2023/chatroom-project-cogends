631823ea895ec319921961cd1d9db58455ea3878
fatemezahrafathian <fathianfatemehzahra@gmail.com>
2023-06-17 12:29:27 +0430

    add ClientHandler class and add constructor

---
+++ server/src/ClientHandler.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+    private String  clientID;
+    public ClientHandler(Socket socket){
+        try {
+            this.socket=socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            System.out.println("SERVER:" + clientUsername+"has connected the chat");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- server/src/Main.java
+++ server/src/Main.java
@@ -6,26 +6,11 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("waiting ");
+//        System.out.println("waiting ");
         ServerSocket serverSocket=new ServerSocket(6666);
-        Socket clientSocket=serverSocket.accept();
-        System.out.println("server accepted");
-        InputStreamReader reader=new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader in=new BufferedReader(reader);
-        String answer="";
-        // System.out.println(answer);
-        while (( answer=in.readLine())!=null) {
-            if(answer.equals("finish")){
-                answer += "tamom shoood";
-                System.out.println(answer);
-                break;
-            }
-            else {
-                answer += "  --> Etelaei Nadaram! ";
-                System.out.println(answer);}
+        while (true){
+            Socket clientSocket=serverSocket.accept();
+            System.out.println("server accepted");
         }
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
     }
 }
