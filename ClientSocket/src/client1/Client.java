ce4d975cf2f210e629be880e69aa51d458057704
ferreshteh <fereshteh.khademi1381@gmail.com>
2023-06-18 12:07:02 +0330

    adding ping method

--- ClientSocket/src/client1/Client.java
+++ ClientSocket/src/client1/Client.java
@@ -11,77 +11,94 @@ public class Client {
     private BufferedWriter bufferedWriter;
 
     //-----------------------------------
-    public  Client(Socket socket,String username){
-        try{
-            this.username=username;
-            this.socket=socket;
-            this.username=username;
-            this.bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    public Client(Socket socket, String username) {
+        try {
+            this.username = username;
+            this.socket = socket;
+            this.username = username;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         } catch (IOException e) {
-closeEverything(socket,bufferedReader,bufferedWriter);        }
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
     }
+
     //----------------------------------
-    public void sendMessage(){
+    public void sendMessage() {
         try {
             bufferedWriter.write(username);
             bufferedWriter.newLine();
             bufferedWriter.flush();
-            Scanner scanner=new Scanner(System.in);
-            while (socket.isConnected()){
-                String messageToSend=scanner.nextLine();
-                bufferedWriter.write(username+": "+messageToSend);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = scanner.nextLine();
+                if (messageToSend.contains("ping")) {
+                    bufferedWriter.write(username + " :" );//----------------------
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                } else {
+                    bufferedWriter.write(username + ": " + messageToSend);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                }
             }
         } catch (IOException e) {
-            closeEverything(socket,bufferedReader,bufferedWriter);
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
     //---------------------------------------
-    public void listenForMassage(){
+    public void listenForMassage() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 String msgFromGroupChat;
-                while (socket.isConnected()){
+                while (socket.isConnected()) {
                     try {
-                        msgFromGroupChat=bufferedReader.readLine();
+                        msgFromGroupChat = bufferedReader.readLine();
                         System.out.println(msgFromGroupChat);
                     } catch (IOException e) {
-                        closeEverything(socket,bufferedReader,bufferedWriter);
+                        closeEverything(socket, bufferedReader, bufferedWriter);
                     }
                 }
             }
         }).start();
     }
+
     //----------------------------------------
-    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
         try {
-            if(bufferedReader !=null){
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(bufferedWriter  !=null){
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-            if(socket != null){
+            if (socket != null) {
                 socket.close();
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     //-----------------------------------------
-    public static void main(String[]args) throws IOException {
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("enter your username to join");
-        String username=scanner.nextLine();
-        Socket socket =new Socket("localhost",1234);
-        Client client = new Client(socket,username);
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", 1234);
+        Client client = new Client(socket, username);
         client.listenForMassage();
         client.sendMessage();
     }
+    //-------------------------
+//     if(massageFromClient.contains("ping")){
+//                        String[] strings = massageFromClient.split("\\s+");
+//                        String username=strings[3];
+//                        for (ClientHandler clientHandler:clientHandlers){
+//                            if (!clientHandler.clientUsername.equals(this.clientUsername)){
+//
+//                            }}
+
 }
--- server/src/ClientHandler.java
+++ server/src/ClientHandler.java
@@ -2,68 +2,89 @@ import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
     public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String clientUsername;
-    private String  clientID;
-    public ClientHandler(Socket socket){
+    private String clientID;
+
+    public ClientHandler(Socket socket) {
         try {
-            this.socket=socket;
+            this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMassage("SERVER:" + clientUsername+"has connected the chat");
+            broadcastMassage("SERVER: " + clientUsername + " has connected the chat ");
         } catch (IOException e) {
-            closeEveryThing(socket,bufferedWriter,bufferedReader);
+            closeEveryThing(socket, bufferedWriter, bufferedReader);
         }
-
     }
 
     @Override
     public void run() {
         String massageFromClient;
-        while (socket.isConnected()){
+        while (socket.isConnected()) {
             try {
-                massageFromClient=bufferedReader.readLine();
+                massageFromClient = bufferedReader.readLine();
                 broadcastMassage(massageFromClient);
             } catch (IOException e) {
-                closeEveryThing(socket,bufferedWriter,bufferedReader);
+                closeEveryThing(socket, bufferedWriter, bufferedReader);
                 break;
             }
         }
     }
-    public void broadcastMassage(String massageToSend){
-        try{
-            for (ClientHandler clientHandler:clientHandlers){
-                if (!clientHandler.clientUsername.equals(this.clientUsername)){
-                    clientHandler.bufferedWriter.write(massageToSend);
-                    clientHandler.bufferedWriter.newLine();
-                    clientHandler.bufferedWriter.flush();
+
+    public void broadcastMassage(String massageToSend) {
+        try {
+            for (ClientHandler clientHandler : clientHandlers) {
+                if (!clientHandler.clientUsername.equals(this.clientUsername)) {
+                    String[] strings = massageToSend.split("\\s+");
+                    String username=strings[0];
+                    if (strings.length == 2) {
+                        sendConnected(username);
+                    } else {
+                        clientHandler.bufferedWriter.write(massageToSend);
+                        clientHandler.bufferedWriter.newLine();
+                        clientHandler.bufferedWriter.flush();
+                    }
                 }
             }
-        }catch (IOException e) {
-            closeEveryThing(socket,bufferedWriter,bufferedReader); throw new RuntimeException(e);
+        } catch (IOException e) {
+            closeEveryThing(socket, bufferedWriter, bufferedReader);
+            throw new RuntimeException(e);
         }
     }
-    public void removeClientHandler(){
+
+    public void removeClientHandler() {
         clientHandlers.remove(this);
-        broadcastMassage("SERVER:" + clientUsername+"has left the chat");
+        broadcastMassage("SERVER:" + clientUsername + "has left the chat");
     }
-    public void closeEveryThing(Socket socket,BufferedWriter bufferedWriter,BufferedReader bufferedReader){
+
+    public void closeEveryThing(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
         removeClientHandler();
         try {
-            if (bufferedReader!= null)
+            if (bufferedReader != null)
                 bufferedReader.close();
-            if (bufferedWriter!= null)
+            if (bufferedWriter != null)
                 bufferedWriter.close();
-            if (socket!= null)
+            if (socket != null)
                 socket.close();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    //----------------------------------------------
+    public void sendConnected(String username) throws IOException {
+        for (ClientHandler clientHandler : clientHandlers) {
+            if (!clientHandler.clientUsername.equals(username)) {
+                clientHandler.bufferedWriter.write("connected");
+                clientHandler.bufferedWriter.newLine();
+                clientHandler.bufferedWriter.flush();
+            }
         }
     }
 }
